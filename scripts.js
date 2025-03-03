6d2fc62dab8b6474fa5df9367620cfcaf4cfbe61
juansemolina777 <106198574+juansemolina777@users.noreply.github.com>
2025-03-03 15:51:38 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
+function createGrid() {
 
+    let gridSize = +prompt("How many grids (0-100) you need in a row:")
+
+    if (gridSize <= 100) {
+        let containerSize = 400;
+        let squareSize = containerSize / gridSize
+        let containerDiv = document.querySelector('#container');
+        containerDiv.innerHTML = "";
+        for (i = 0; i < gridSize * gridSize; i++) {
+            let divs = document.createElement('div');
+            divs.classList.add('divs')
+            divs.style.height = `${squareSize}px`
+            divs.style.width = `${squareSize}px`
+            containerDiv.appendChild(divs);
+        }
+    } else {
+        alert("Enter a Number Less than 100.")
+    }
+    let divs = document.querySelectorAll('.divs');
+    divs.forEach(divs => {
+        divs.addEventListener('mouseover', () => {
+            divs.style.backgroundColor = 'rgb(1, 115, 255)'
+        })
+    })
